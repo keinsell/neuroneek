14b31a8675dd376b7a40926462a21943caab7cff
Jakub Olan <keinsell@protonmail.com>
2024-04-27 18:52:48 +0200

    refactor: ♻️ add ingestion form enhancements and improve toast notifications
    Improved the ingestion form UI by adding comprehensive error handling and highlighting on problematic input fields. Additionally, enhanced the toast notifications system by introducing a new sonner package and displaying detailed success messages that includes submitted data. The form now also includes both client-side and server-side valibot validation.

--- apps/web/src/lib/actions/add-ingestion.ts
+++ apps/web/src/lib/actions/add-ingestion.ts
@@ -1,37 +1,37 @@
 // This aciton will save the ingestion to local state, then local storage and
 // in a free time request will be sent to the server to save the ingestion.
 
-import { useIngestionStore } from '@/lib/store/ingestion-store';
-import { date, enum_, number, object, Output, string, uuid } from 'valibot';
-import { RouteOfAdministrationClassification } from '@/types/route-of-administration';
+import { useIngestionStore } from '@/lib/store/ingestion-store'
+import { RouteOfAdministrationClassification } from '@/types/route-of-administration'
+import { enum_, number, object, Output, string } from 'valibot'
 
-const AddIngestionCommand = object({
-  substanceId: string([uuid()]),
-  routeOfAdministration: enum_(RouteOfAdministrationClassification),
-  dosage: object({
-    amount: number(),
-    unit: string(),
-  }),
-  ingestedAt: date(),
-});
+export const AddIngestionCommand = object({
+	substanceId: string([]),
+	routeOfAdministration: enum_(RouteOfAdministrationClassification),
+	dosage: object({
+		amount: number()
+		// unit: string()
+	})
+	// ingestedAt: date()
+})
 
-export type AddIngestionCommand = Output<typeof AddIngestionCommand>;
+export type AddIngestionCommand = Output<typeof AddIngestionCommand>
 
 export const addIngestion = (ingestion: AddIngestionCommand) => {
-  console.log(ingestion);
+	console.log(ingestion)
 
-  // Fetch all ingestion's from the store
-  const currentIngestion = useIngestionStore.getState().ingestions;
+	// Fetch all ingestion's from the store
+	const currentIngestion = useIngestionStore.getState().ingestions
 
-  // Add the new ingestion to the store
-  useIngestionStore.getState().addIngestion({
-    id: '',
-    substanceId: ingestion.substanceId,
-    routeOfAdministration: ingestion.routeOfAdministration,
-    dosage: ingestion.dosage,
-    ingestedAt: ingestion.ingestedAt,
-  } as any);
+	// Add the new ingestion to the store
+	useIngestionStore.getState().addIngestion({
+		id: '',
+		substanceId: ingestion.substanceId,
+		routeOfAdministration: ingestion.routeOfAdministration,
+		dosage: ingestion.dosage
+		// ingestedAt: ingestion.ingestedAt
+	} as any)
 
-  // Save the ingestions to local storage
-  localStorage.setItem('ingestions', JSON.stringify(currentIngestion));
-};
+	// Save the ingestions to local storage
+	localStorage.setItem('ingestions', JSON.stringify(currentIngestion))
+}
--- apps/web/src/pages/ingestion/create.tsx
+++ apps/web/src/pages/ingestion/create.tsx
@@ -1,126 +1,158 @@
-import { Label } from '@/components/ui/label';
+import AppLayout from '@/components/layouts/app-layout'
+import { Button } from '@/components/ui/button'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { roaSeed } from '@/lib/store/roas-store';
-import { Textarea } from '@/components/ui/textarea';
-import AppLayout from '@/components/layouts/app-layout';
-import { ChevronDownIcon, PillIcon } from 'lucide-react';
-import { SUBSTANCES_MOCKUP } from '@/types/substance';
-import { getValue, setValue, SubmitHandler, useForm } from '@modular-forms/react';
-import { addIngestion, AddIngestionCommand } from '@/lib/actions/add-ingestion';
-import { useToast } from '@/components/ui/use-toast';
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { FormMessageV2 } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { useToast } from '@/components/ui/use-toast'
+import { addIngestion, AddIngestionCommand } from '@/lib/actions/add-ingestion'
+import { roaSeed } from '@/lib/store/roas-store'
+import { SUBSTANCES_MOCKUP } from '@/types/substance'
+import { getValue, setValue, SubmitHandler, useForm } from '@modular-forms/react'
+import { ChevronDownIcon, PillIcon } from 'lucide-react'
 
 type CreateIngestionForm = AddIngestionCommand
 
 export default function CreateIngestionPage() {
-  const [addIngestionForm, { Form, Field }] = useForm<CreateIngestionForm>();
-  const { toast } = useToast();
+	const [addIngestionForm, { Form, Field }] = useForm<CreateIngestionForm>()
+	// const [addIngestionForm, { Form, Field }] = useForm<CreateIngestionForm>({ validate: valiForm(AddIngestionCommand) })
+	const { toast } = useToast()
 
+	const ingestionSubmitHandler: SubmitHandler<CreateIngestionForm> = (values, event) => {
+		event.preventDefault()
+		toast({
+			title: '🎉 Ingestion Logged',
+			description: (
+				<div className='flex flex-col gap-2'>
+					<div>
+						<p className='text-gray-500'>You have successfully logged an ingestion.</p>
+						<p className='text-gray-500'>You can view the ingestion below.</p>
+					</div>
+					<pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+						<code className='text-white'>{JSON.stringify(values, null, 2)}</code>
+					</pre>
+				</div>
+			)
+		})
+		addIngestion(values)
+	}
 
-  const ingestionSubmitHandler: SubmitHandler<CreateIngestionForm> = (values, event) => {
-    event.preventDefault();
-    addIngestion(values);
-
-
-    toast({
-      title: 'Ingestion logged successfully',
-      description: 'The ingestion has been logged successfully.',
-      type: 'foreground',
-    });
-  };
-
-  return (
-    <AppLayout>
-      <div className="flex items-center">
-        <h1 className="font-semibold text-lg md:text-2xl">Log Ingestion</h1>
-      </div>
-      <div className="border shadow-sm rounded-lg p-6">
-        <Form className="grid gap-6" onSubmit={ingestionSubmitHandler}>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <Label htmlFor="substance">Substance</Label>
-              <Field name="substanceId" type="string">
-                {(field, props) => <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button className="w-full flex items-center justify-between" variant="outline">
-                      <span> {
-                        getValue(addIngestionForm, 'substanceId') ? (SUBSTANCES_MOCKUP.find((substance) => `${substance.id}` === getValue(addIngestionForm, 'substanceId')))?.name : 'Search for a substance'
-                      }</span>
-                      <ChevronDownIcon className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-full max-h-[300px] overflow-auto">
-                    <div className="px-4 py-2">
-                      <Input className="w-full" placeholder="Select a substance..." />
-                    </div>
-                    <DropdownMenuSeparator />
-                    {SUBSTANCES_MOCKUP.map((substance) => (
-                      <DropdownMenuItem key={substance.id} itemID={`${substance.id}`}
-                                        onClick={() => setValue(addIngestionForm, 'substanceId', `${substance.id}`)}>{substance.name}</DropdownMenuItem>
-                    ))}
-                  </DropdownMenuContent>
-                </DropdownMenu>}
-              </Field>
-            </div>
-            <Field name="dosage.amount" type="number">
-              {(field, props) => <>
-                <Label htmlFor="dosage">Dosage</Label>
-                <Input id="dosage" placeholder="Enter dosage" type="number"
-                       onChange={(e) => setValue(addIngestionForm, 'dosage.amount', parseInt(e.target.value))} />
-              </>}
-            </Field>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            {/* Route of Administration FormElement*/}
-            <div>
-              <Label htmlFor="roa">Route of Administration</Label>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button className="w-full flex items-center justify-between" variant="outline">
-                    <span>Select a route</span>
-                    <ChevronDownIcon className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-full">
-                  {roaSeed.map((roa) => (
-                    <DropdownMenuItem key={roa.id} itemID={roa.id}>{roa.name}</DropdownMenuItem>
-                  ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <div>
-              <Label htmlFor="date">Date Administered</Label>
-              <Input id="date" type="date" />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <Label htmlFor="time">Time Administered</Label>
-              <Input id="time" type="time" />
-            </div>
-            <div>
-              <Label htmlFor="notes">Notes</Label>
-              <Textarea id="notes" placeholder="Enter any additional notes" />
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <PillIcon className="h-6 w-6 text-gray-500" />
-              <span className="text-sm font-medium text-gray-500">Dosage Strength: 100 μg</span>
-            </div>
-            <Button className="ml-auto" type="submit">
-              Log Ingestion
-            </Button>
-          </div>
-        </Form>
-      </div>
-    </AppLayout>
-  );
+	return (
+		<AppLayout>
+			<div className='flex items-center'>
+				<h1 className='font-semibold text-lg md:text-2xl'>Log Ingestion</h1>
+			</div>
+			<div className='border shadow-sm rounded-lg p-6'>
+				<Form className='grid gap-6' onSubmit={ingestionSubmitHandler}>
+					<div className='grid grid-cols-2 gap-4'>
+						<div>
+							<Field name='substanceId' type='string'>
+								{(field, props) => (
+									<>
+										<Label htmlFor='substance'>Substance</Label>
+										<DropdownMenu>
+											<DropdownMenuTrigger asChild>
+												<Button className='w-full flex items-center justify-between' variant='outline'>
+													<span>
+														{getValue(addIngestionForm, 'substanceId')
+															? SUBSTANCES_MOCKUP.find(
+																	substance => `${substance.id}` === getValue(addIngestionForm, 'substanceId')
+															  )?.name
+															: 'Search for a substance'}
+													</span>
+													<ChevronDownIcon className='h-4 w-4' />
+												</Button>
+											</DropdownMenuTrigger>
+											{field.error && <FormMessageV2>{field.error}</FormMessageV2>}
+											<DropdownMenuContent className='w-full max-h-[300px] overflow-auto'>
+												<div className='px-4 py-2'>
+													<Input className='w-full' placeholder='Select a substance...' />
+												</div>
+												<DropdownMenuSeparator />
+												{SUBSTANCES_MOCKUP.map(substance => (
+													<DropdownMenuItem
+														key={substance.id}
+														itemID={`${substance.id}`}
+														onClick={() => setValue(addIngestionForm, 'substanceId', `${substance.id}`)}>
+														{substance.name}
+													</DropdownMenuItem>
+												))}
+											</DropdownMenuContent>
+										</DropdownMenu>
+									</>
+								)}
+							</Field>
+						</div>
+						<div>
+							<Field name='dosage.amount' type='number'>
+								{(field, props) => (
+									<>
+										<Label htmlFor='dosage'>Dosage</Label>
+										<Input
+											id='dosage'
+											placeholder='Enter dosage'
+											type='number'
+											onChange={e => setValue(addIngestionForm, 'dosage.amount', parseInt(e.target.value))}
+										/>
+										{field.error && <FormMessageV2>{field.error}</FormMessageV2>}
+									</>
+								)}
+							</Field>
+						</div>
+					</div>
+					<div className='grid grid-cols-2 gap-4'>
+						{/* Route of Administration FormElement*/}
+						<div>
+							<Label htmlFor='roa'>Route of Administration</Label>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<Button className='w-full flex items-center justify-between' variant='outline'>
+										<span>Select a route</span>
+										<ChevronDownIcon className='h-4 w-4' />
+									</Button>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent className='w-full'>
+									{roaSeed.map(roa => (
+										<DropdownMenuItem key={roa.id} itemID={roa.id}>
+											{roa.name}
+										</DropdownMenuItem>
+									))}
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</div>
+						<div>
+							<Label htmlFor='date'>Date Administered</Label>
+							<Input id='date' type='date' />
+						</div>
+					</div>
+					<div className='grid grid-cols-2 gap-4'>
+						<div>
+							<Label htmlFor='time'>Time Administered</Label>
+							<Input id='time' type='time' />
+						</div>
+						<div>
+							<Label htmlFor='notes'>Notes</Label>
+							<Textarea id='notes' placeholder='Enter any additional notes' />
+						</div>
+					</div>
+					<div className='flex items-center justify-between'>
+						<div className='flex items-center gap-2'>
+							<PillIcon className='h-6 w-6 text-gray-500' />
+							<span className='text-sm font-medium text-gray-500'>Dosage Strength: 100 μg</span>
+						</div>
+						<Button className='ml-auto' type='submit'>
+							Log Ingestion
+						</Button>
+					</div>
+				</Form>
+			</div>
+		</AppLayout>
+	)
 }
