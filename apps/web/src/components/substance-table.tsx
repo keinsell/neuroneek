94a5817b13863ebfe21716ece34b44e07b170fc1
Jakub Olan <keinsell@protonmail.com>
2024-04-30 03:05:56 +0200

    refactor: ♻️ add skeleton when table is loading

--- apps/web/src/components/substance-table.tsx
+++ apps/web/src/components/substance-table.tsx
@@ -1,15 +1,14 @@
+import { Skeleton } from '@/components/ui/skeleton'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { useStore } from '@/stores/use-store'
 import { type Substance } from '@/types/substance'
 import React from 'react'
 
 interface SubstanceTableProperties {
 	substances: Substance[]
+	isLoading: boolean
 }
 
-export default function SubstanceTable({ substances }: SubstanceTableProperties) {
-	useStore.setState({ substances })
-	const { substances: substanceState } = useStore()
+export default function SubstanceTable({ substances, isLoading }: SubstanceTableProperties) {
 	return (
 		<Table>
 			<TableHeader>
@@ -18,11 +17,21 @@ export default function SubstanceTable({ substances }: SubstanceTableProperties)
 				</TableRow>
 			</TableHeader>
 			<TableBody>
-				{substanceState.map(substance => (
-					<TableRow key={substance.id}>
-						<TableCell className='font-medium'>{substance.name}</TableCell>
-					</TableRow>
-				))}
+				{isLoading
+					? Array(10)
+							.fill(0)
+							.map((_, index) => (
+								<TableRow key={index}>
+									<TableCell>
+										<Skeleton className='h-4 w-[150px]' />
+									</TableCell>
+								</TableRow>
+							))
+					: substances.map(substance => (
+							<TableRow key={substance.id}>
+								<TableCell className='font-medium'>{substance.name}</TableCell>
+							</TableRow>
+					  ))}
 			</TableBody>
 		</Table>
 	)
