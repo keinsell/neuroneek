4e298b48ce7b2b37f716077695df538dfbc0e8ee
Jakub Olan <keinsell@protonmail.com>
2024-05-05 03:32:56 +0200

    refactor: ♻️ add account deletion

--- apps/server/src/routes/v1/account.ts
+++ apps/server/src/routes/v1/account.ts
@@ -1,100 +1,132 @@
-import {BadRequestException, Body, Controller, HttpCode, HttpStatus, Logger, Post} from '@nestjs/common';
-import {JwtService} from '@nestjs/jwt';
-import {ApiOperation, ApiProperty, ApiTags} from "@nestjs/swagger"
-import {hash} from "@node-rs/argon2"
-import {nanoid} from "nanoid"
-import {Account} from "../../_gen/account"
-import {PrismaService} from "../../core/modules/database/prisma/services/prisma-service"
+import { BadRequestException, Body, Controller, HttpCode, HttpStatus, Logger, Post, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ApiBearerAuth, ApiOperation, ApiProperty, ApiTags } from "@nestjs/swagger"
+import { hash } from "@node-rs/argon2"
+import { nanoid } from "nanoid"
+import { Account } from "../../_gen/account"
+import { PrismaService } from "../../core/modules/database/prisma/services/prisma-service"
+import { GetUser } from '../../core/identity/authn/jwt-authentication-strategy';
+import { JwtAuthorizationGuard } from '../../core/identity/authn/components/guards/jwt-authorization-guard';
 
 
 export class AccountUsernameTaken
-    extends BadRequestException
-    {
-        constructor()
-            {
-                super('Username already exists');
-            }
-    }
+  extends BadRequestException {
+  constructor() {
+    super('Username already exists');
+  }
+}
 
 
-export class AccountRegistered
-    {
-        id: string   = nanoid();
-        name: string = "account.registered";
+export class AccountRegistered {
+  id: string = nanoid();
+  name: string = "account.registered";
 
-        accountId: string;
-        accountUsername: string;
-
-
-        constructor(accountId: string, username: string)
-            {
-                this.accountId       = accountId;
-                this.accountUsername = username;
-            }
+  accountId: string;
+  accountUsername: string;
 
 
-        get description()
-            {
-                return `Account '${this.accountId}' with username '${this.accountUsername}' was registered successfully!`;
-            }
-    }
+  constructor(accountId: string, username: string) {
+    this.accountId = accountId;
+    this.accountUsername = username;
+  }
 
 
-export class RegisterAccount
-    {
-        @ApiProperty({example: "elon_musk", description: "The user's unique username"}) username!: string;
-        @ApiProperty({example: "ISendCarsIntoFuckingSpace", description: "The user's password"}) password!: string;
-    }
+  get description() {
+    return `Account '${this.accountId}' with username '${this.accountUsername}' was registered successfully!`;
+  }
+}
+
+export class AccountDeleted {
+  id: string = nanoid();
+  name: string = "account.deleted";
+  description: string;
+
+  constructor(accountId: string) {
+    this.description = `Account '${accountId}' was deleted successfully!`;
+  }
+}
+
+
+export class RegisterAccount {
+  @ApiProperty({ example: "elon_musk", description: "The user's unique username" }) username!: string;
+  @ApiProperty({ example: "ISendCarsIntoFuckingSpace", description: "The user's password" }) password!: string;
+}
 
 @ApiTags("Account Management")
 @Controller('account')
-export class AccountController
-    {
-        private logger: Logger = new Logger("account.controller")
+export class AccountController {
+  private logger: Logger = new Logger("account.controller")
 
 
-        constructor(private readonly prismaService: PrismaService, private readonly jwtService: JwtService) {}
+  constructor(private readonly prismaService: PrismaService, private readonly jwtService: JwtService) { }
 
 
-        @ApiOperation({
-                          summary    : 'Register account',
-                          description: `Operation will register a new account in application allowing user to interact with his namespace.`,
-                      }) @Post('register') @HttpCode(HttpStatus.OK)
-        async register(@Body() body: RegisterAccount): Promise<Account>
-            {
-                if (!body?.password || !body?.username)
-                    {
-                        throw new BadRequestException('Username and password are required');
-                    }
-
-                // Find account with given username and throw error if it exists
-                const account = await this.prismaService.account.findUnique({
-                                                                                where: {
-                                                                                    username: body.username,
-                                                                                },
-                                                                            });
-
-                if (account)
-                    {
-                        throw new AccountUsernameTaken();
-                    }
-
-                // Hash the password and store the user into the database
-                const hashPassword = await hash(body.password);
-
-                const user = await this.prismaService.account.create({
-                                                                         data: {
-                                                                             username: body.username,
-                                                                             password: hashPassword,
-                                                                         },
-                                                                     });
-
-                const accountRegistered = new AccountRegistered(user.id, user.username)
-
-                this.logger.log(accountRegistered.description)
-
-                return {
-                    ...user,
-                };
-            }
+  @ApiOperation({
+    summary: 'Register account',
+    description: `Operation will register a new account in application allowing user to interact with his namespace.`,
+  }) @Post('register') @HttpCode(HttpStatus.OK)
+  async register(@Body() body: RegisterAccount): Promise<Account> {
+    if (!body?.password || !body?.username) {
+      throw new BadRequestException('Username and password are required');
     }
+
+    // Find account with given username and throw error if it exists
+    const account = await this.prismaService.account.findUnique({
+      where: {
+        username: body.username,
+      },
+    });
+
+    if (account) {
+      throw new AccountUsernameTaken();
+    }
+
+    // Hash the password and store the user into the database
+    const hashPassword = await hash(body.password);
+
+    const user = await this.prismaService.account.create({
+      data: {
+        username: body.username,
+        password: hashPassword,
+      },
+    });
+
+    const accountRegistered = new AccountRegistered(user.id, user.username)
+
+    this.logger.log(accountRegistered.description)
+
+    return {
+      ...user,
+    };
+  }
+
+  @UseGuards(JwtAuthorizationGuard)
+  @ApiOperation({
+    summary: "Delete account",
+    description: `Operation will delete account from the system. This operation is irreversible.`
+  }) @Post('delete') @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  async deleteAccount(@Body() body: { accountId?: string }, @GetUser() user: Account) {
+    let accountId = body.accountId;
+
+    if (body.accountId) {
+      if (user.id !== body.accountId) {
+        throw new BadRequestException('You can only delete your own account');
+      }
+    } else {
+      accountId = user.id;
+    }
+
+    await this.prismaService.account.delete({
+      where: {
+        id: accountId,
+      },
+    });
+
+    const accountDeleted = new AccountDeleted(accountId!)
+
+    this.logger.log(accountDeleted.description)
+
+    return accountDeleted;
+  }
+}
--- apps/server/src/routes/v1/subject.ts
+++ apps/server/src/routes/v1/subject.ts
@@ -1,42 +1,110 @@
-import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
-import { ApiOperation, ApiProperty, ApiTags } from "@nestjs/swagger";
+import {Body, Controller, Delete, Get, Logger, Post, Put, UseGuards} from "@nestjs/common";
+import {ApiBearerAuth, ApiOperation, ApiProperty, ApiTags} from "@nestjs/swagger";
+import {Prisma} from "@neuronek/db/generated/prisma-client";
+import {Account} from "db";
+import {Subject} from "../../_gen/subject";
+import {JwtAuthorizationGuard} from "../../core/identity/authn/components/guards/jwt-authorization-guard";
+import {GetUser} from "../../core/identity/authn/jwt-authentication-strategy";
+import {PrismaService} from "../../core/modules/database/prisma/services/prisma-service";
 
-export class CreateSubject {
-  @ApiProperty({ name: "givenName", description: "Given name of the subject", required: false })
-  givenName?: string;
+export class CreateSubject
+    {
+        @ApiProperty({name: "givenName", description: "Given name of the subject", required: false, example: "Albert"})
+        givenName?: string;
 
-  @ApiProperty({ name: "familyName", description: "Family name of the subject", required: false })
-  familyName?: string;
+        @ApiProperty(
+            {name: "familyName", description: "Family name of the subject", required: false, example: "Einstein"})
+        familyName?: string;
 
-  @ApiProperty({ name: "birthDate", description: "Birth date of the subject", required: false })
-  birthDate?: Date;
-}
+        @ApiProperty({
+                         name       : "birthDate",
+                         description: "Birth date of the subject",
+                         required   : false,
+                         example    : "1879-03-14T00:00:00.000Z"
+                     })
+        birthDate?: Date;
+    }
 
 @ApiTags('Subject Profile')
 @Controller('subject')
-export class SubjectController {
+export class SubjectController
+    {
 
-  @ApiOperation({ summary: '🚧 Create subject', description: 'Creates a new subject for currently authenticated account. We recommend using only one subject profile for now as using multiple ones can lead to unexpected bugs.', operationId: 'create-subject' })
-  @Post()
-  async createSubject() {
-    return "Subject created";
-  }
+        private logger = new Logger(SubjectController.name);
 
-  @ApiOperation({ summary: '🚧 Get subject', description: 'Retrieves the subject profile for currently authenticated account.', operationId: 'get-subject' })
-  @Get()
-  async getSubject() {
-    return "Subject retrieved";
-  }
+        constructor(
+            private readonly prismaService: PrismaService,
+        )
+            {}
 
-  @ApiOperation({ summary: '🚧 Update subject', description: 'Updates the subject profile for currently authenticated account.', operationId: 'update-subject' })
-  @Put()
-  async updateSubject() {
-    return "Subject updated";
-  }
+        @ApiBearerAuth()
+        @UseGuards(JwtAuthorizationGuard)
+        @ApiOperation({
+                          summary    : '🚧 Create subject',
+                          description: 'Creates a new subject for currently authenticated account. We recommend using only one subject profile for now as using multiple ones can lead to unexpected bugs.',
+                          operationId: 'create-subject'
+                      })
+        @Post()
+        async createSubject(
+            @Body() createSubject: CreateSubject,
+            @GetUser() user: Account,
+        ): Promise<Subject>
+            {
+                // Validate input for createSubject
 
-  @ApiOperation({ summary: '🚧 Delete subject', description: 'Deletes the subject profile for currently authenticated account.', operationId: 'delete-subject' })
-  @Delete()
-  async deleteSubject() {
-    return "Subject deleted";
-  }
-}
+                // Create subject
+                const prismaCreateSubject: Prisma.SubjectCreateInput = {
+                    firstName  : createSubject.givenName,
+                    lastName   : createSubject.familyName,
+                    dateOfBirth: createSubject.birthDate,
+                    account    : {
+                        connect: {
+                            id: user.id
+                        }
+                    }
+                }
+
+                // Create subject
+                const subject = await this.prismaService.subject.create({
+                                                                            data: prismaCreateSubject
+                                                                        });
+
+
+                this.logger.debug(`Created subject: ${JSON.stringify(subject)}`)
+
+                return subject as any;
+            }
+
+        @ApiOperation({
+                          summary    : '🚧 Get subject',
+                          description: 'Retrieves the subject profile for currently authenticated account.',
+                          operationId: 'get-subject'
+                      })
+        @Get()
+        async getSubject()
+            {
+                return "Subject retrieved";
+            }
+
+        @ApiOperation({
+                          summary    : '🚧 Update subject',
+                          description: 'Updates the subject profile for currently authenticated account.',
+                          operationId: 'update-subject'
+                      })
+        @Put()
+        async updateSubject()
+            {
+                return "Subject updated";
+            }
+
+        @ApiOperation({
+                          summary    : '🚧 Delete subject',
+                          description: 'Deletes the subject profile for currently authenticated account.',
+                          operationId: 'delete-subject'
+                      })
+        @Delete()
+        async deleteSubject()
+            {
+                return "Subject deleted";
+            }
+    }
