bb977762e73bd405d959e5d295c3ba5bbf954a5b
Jakub Olan <keinsell@protonmail.com>
2024-05-13 04:03:13 +0200

    refactor: ♻️ add substance-combobox.tsx

--- apps/web/src/components/substance-combobox.tsx
+++ apps/web/src/components/substance-combobox.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Command, CommandEmpty, CommandInput, CommandItem } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Combobox } from '@/components/combobox'
 
 export function SubstanceCombobox() {
 	const [open, setOpen] = useState(false);
@@ -18,35 +19,12 @@ export function SubstanceCombobox() {
 	];
 
 	return (
-		<Popover open={open} onOpenChange={setOpen}>
-			<PopoverTrigger asChild>
-				<Button
-					variant="outline"
-					role="combobox"
-					aria-expanded={open}
-					className="w-[200px] justify-between"
-				>
-					{value ? frameworks.find(f => f.value === value)?.label : "Select framework..."}
-					<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-[200px] p-0">
-				<Command value={value} onValueChange={setValue}>
-					<CommandInput placeholder="Search framework..." />
-					<CommandEmpty>No framework found.</CommandEmpty>
-					{frameworks.map(framework => (
-						<CommandItem key={framework.value} value={framework.value}>
-							<Check
-								className={cn(
-									"mr-2 h-4 w-4",
-									value === framework.value ? "opacity-100" : "opacity-0"
-								)}
-							/>
-							{framework.label}
-						</CommandItem>
-					))}
-				</Command>
-			</PopoverContent>
-		</Popover>
+		<Combobox
+			mode='single' //single or multiple
+			options={[]}
+			placeholder='Select option...'
+			selected={""} // string or array
+			onChange={(value) => console.log(value)}
+		/>
 	);
 }
