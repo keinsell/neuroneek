0e637c7a32a56789817310f01c14829fda9e7d7a
Jakub Olan <keinsell@protonmail.com>
2024-05-16 05:56:59 +0200

    ci: ♻️ use dagger for ci abstraction

--- dagger/main.go
+++ dagger/main.go
@@ -14,37 +14,10 @@
 
 package main
 
-import (
-	"context"
-)
-
 type Neuronek struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *Neuronek) ContainerEcho(stringArg string) *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Neuronek) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
-
+// Build a monorepository with pnpm
 func (m *Neuronek) Build(source *Directory) *Container {
-	node := dag.Node()
-	node = m.setupPnpm(node)
-	return node.Container()
-}
-
-func (m *Neuronek) buildBaseImage(source *Directory) *Container {
-	return dag.Node().WithPackageManager("pnpm").Container()
-}
-
-func (m *Neuronek) setupPnpm(node *Node) *Node {
-	return node.WithPackageManager("pnpm")
+	node := dag.Node().WithPackageManager("pnpm").Container()
+	return node
 }
