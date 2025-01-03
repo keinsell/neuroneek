544bd2ea087a50ab5e6f516a238d0f2e97394a4b
keinsell <keinsell@protonmail.com>
2025-01-03 22:05:56 +0100

    format floats in dosage strings
    Use correct approach into formatting float values during converting them to strings with up-scaling to complete values.

--- src/lib/dosage.rs
+++ src/lib/dosage.rs
@@ -1,17 +1,25 @@
-use measurements::Mass;
+use clap::builder::TypedValueParser;
+use float_pretty_print::PrettyPrintFloat;
+use measurements::{Mass, Measurement};
 use miette::IntoDiagnostic;
 use std::str::FromStr;
 
 pub type Dosage = Mass;
 
-// TODO: This is not what it's meant to be, there is a still work needed on sane
-// approach to automatic scaling of units.
 /// Function will take human-readable input as representation of mass of
 /// substance that was ingested (also referred as Dosage)
 pub fn parse_dosage(input: &str) -> miette::Result<Dosage>
 {
     Mass::from_str(input).into_diagnostic()
 }
+
+pub fn format_dosage(input: &Dosage) -> miette::Result<String> {
+    let suggested_unit = input.get_appropriate_units();
+    let float = format!("{:4.4}", PrettyPrintFloat(suggested_unit.1));
+    let unit = suggested_unit.0;
+    Ok(format!("{} {}", float.trim_start(), unit))
+}
+
 
 #[cfg(test)]
 mod tests
@@ -27,5 +35,11 @@ mod tests
             parse_dosage("100ug").unwrap(),
             Mass::from_micrograms(100f64)
         )
+    }
+
+    #[test]
+    fn test_print_dosage() {
+        let dosage = Mass::from_grams(0.1);
+        assert_eq!(format_dosage(&dosage).unwrap(), "100 mg");
     }
 }
--- src/view_model/ingestion.rs
+++ src/view_model/ingestion.rs
@@ -1,6 +1,8 @@
+use crate::lib::dosage::format_dosage;
 use crate::lib::dosage::Dosage;
 use crate::lib::orm::ingestion;
 use crate::lib::output::Formatter;
+use core::convert::From;
 use measurements::Measurement;
 use serde::Deserialize;
 use serde::Serialize;
@@ -26,11 +28,13 @@ impl From<ingestion::Model> for ViewModel
 {
     fn from(model: ingestion::Model) -> Self
     {
+        let dosage = Dosage::from_base_units(model.dosage.into());
+
         Self::builder()
             .id(model.id)
             .substance_name(model.substance_name)
             .route(model.route_of_administration)
-            .dosage(Dosage::from_base_units(model.dosage as f64).to_string())
+            .dosage(format_dosage(&dosage).unwrap())
             .ingested_at(model.ingested_at.to_string())
             .build()
     }
